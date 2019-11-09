3b8316beb3ca77cd11f647fe0887e3425c1bb10b
glucianog <glgomes@sga.pucminas.br>
2019-11-09 18:26:51 -0300

    trying new design

--- frontend/src/components/CardPokemon.tsx
+++ frontend/src/components/CardPokemon.tsx
@@ -19,22 +19,20 @@ function CardPokemon({ pokemon }, isLocked) {
             >
                 <Image source={{uri: 'http://i.imgur.com/91AR0Lo.jpg'}} style={styles.cardImage} />
                 <View>
-                <Image 
-                    source={ (isLocked) ? require('../assets/locked_padlock.png') :
-                        require('../assets/unlocked_padlock.png') }
-                    style={styles.padlock}                
-                />
-                <TouchableOpacity 
-                    activeOpacity={ .5 } 
-                    onPress={capturePokemon} 
-                >                    
                     <Image 
-                        source={ require('../assets/pokeball.jpg') }
-                        style={styles.pokeball}                
+                        source={ (isLocked) ? require('../assets/locked_padlock.png') :
+                            require('../assets/unlocked_padlock.png') }
+                        style={styles.padlock}                
                     />
-                </TouchableOpacity>
-                    <Text style={styles.textLeft}>Rabbit, 10</Text>
-                    <Text style={styles.textRight}>1 Connection</Text>
+                    <TouchableOpacity 
+                        activeOpacity={ .5 } 
+                        onPress={ capturePokemon }  
+                    >                    
+                        <Image 
+                            source={ require('../assets/pokeball.jpg') }
+                            style={styles.pokeball}                
+                        />
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>  
@@ -51,33 +49,27 @@ const styles = StyleSheet.create ({
         borderWidth: 2,
         borderRadius: 3,
         borderColor: '#000',
-        width: 350,
-        height: 400,
-        padding: 5
+        width: 300,
+        height: 300,
+        padding: 10
     },
     cardImage: {
         height: 250,
     },
     padlock: {
+        position: 'absolute',
+        left: 0,
+        top: 1,
         height: 50,
         width: 50,
-        alignSelf: 'flex-start'
     },
     pokeball: {
-        height: 50,
-        width: 50,
-        alignSelf: 'flex-end'
-    },
-    textLeft: {
-        position: 'absolute',
-        left:0,
-        top:0
-    },
-    textRight: {
         position: 'absolute',
         right: 0,
-        top: 0
-    ,}
+        top: 1,
+        height: 50,
+        width: 50,
+    },
 })
 
 export default withNavigation(CardPokemon);
