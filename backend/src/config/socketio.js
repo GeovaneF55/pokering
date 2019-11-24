3c9a798ad76d581074e52191d99a4e31b53669d9
Luigi D. C. Soares <luigi.soares@sga.pucminas.br>
2019-11-24 16:31:43 -0300

    backend

--- backend/src/config/socketio.js
+++ backend/src/config/socketio.js
@@ -1,6 +1,6 @@
+const fetch = require('node-fetch')
 const socketio = require('socket.io')
 
-let pokemon = 'Bulbassalto'
 let nodes = []
 let mapIndex = new Map()
 let current = 0
@@ -12,57 +12,78 @@ const moveToken = () => {
     }
 
     const last = current
-    console.log("LENGTH:", nodes.length)
     current = (current + 1) % nodes.length
 
-    console.log('LAST:', last, '\tCURRENT:', current)
-    nodes.forEach(u => console.log(u.id))
-
+    // Invalidate token of the current node.
     nodes[last].emit('token', false)
+
+    // Send token to the next node.
     nodes[current].emit('token', true)
+}
+
+// We're considering only the first generation.
+const lastID = 151
+const pokemonAPI = `https://pokeapi.co/api/v2/pokemon/`
+const randomPokemon = () => {
+    const randomID = Math.floor(Math.random() * lastID)
+    const url = pokemonAPI + randomID
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            return {
+                name: data.name,
+                url: data.sprites.front_default,
+            }
+        })
+        .catch(({ message }) => console.log(message))
 }
 
 module.exports = server => {
     const io = socketio(server)
+    randomPokemon().then(next => {
+        let pokemon = next
 
-    io.on('connection', socket => {
-        nodes.push(socket)
-        socket.emit('pokemon', pokemon)
+        io.on('connection', socket => {
+            nodes.push(socket)
+            socket.emit('pokemon', pokemon)
 
-        let index = nodes.length - 1
-        mapIndex.set(socket.id, index)
+            let index = nodes.length - 1
+            mapIndex.set(socket.id, index)
 
-        console.log('CONECTADO:', socket.id)
+            console.log('CONNECTED:', socket.id)
 
-        socket.once('disconnect', () => {
-            console.log('DISCONNECT BABY:', socket.id)
-            const isTokenOwner = socket === nodes[current]
+            socket.once('disconnect', () => {
+                const isTokenOwner = socket === nodes[current]
 
-            // Remove the node and update the mapIndex.
-            nodes.splice(mapIndex.get(socket.id), 1)
-            console.log('ID SENDO DELETADO:', socket.id)
-            for (const [ key, value ] of mapIndex) {
-                console.log(key + " = " + value)
-            }
+                // Remove the node and update the mapIndex.
+                nodes.splice(mapIndex.get(socket.id), 1)
+                mapIndex.delete(socket.id)
+                nodes.forEach((i, n) => mapIndex.set(n, i))
 
-            nodes.forEach(n => console.log(n.id))
-            mapIndex.delete(socket.id)
-            nodes.forEach((i, n) => mapIndex.set(n, i))
+                // Send token to the next node if the current was the disconnected one.
+                if (isTokenOwner && nodes.length > 0) {
+                    current %= nodes.length
+                    nodes[current].emit('token', true)
+                }
 
-            // Send token to the next node if the current was the disconnected one.
-            console.log(current, isTokenOwner, nodes.length)
-            if (isTokenOwner && nodes.length > 0) {
-                current %= nodes.length
-                console.log(current, isTokenOwner, nodes.length, nodes[current].id)
-                nodes[current].emit('token', true)
+                console.log('DISCONNECTED:', socket.id)
+            })
+
+            socket.on('captured', () => {
+                randomPokemon().then(next => {
+                    pokemon = next
+                    io.emit('pokemon', pokemon)
+                })
+            })
+
+            // Send the initial token to the first node.
+            if (nodes.length <= 1) {
+                socket.emit('token', true)
+                return
             }
         })
 
-        if (nodes.length <= 1) {
-            socket.emit('token', true)
-            return
-        }
+        setInterval(moveToken, timeout)
     })
-
-    setInterval(moveToken, timeout)
 }
